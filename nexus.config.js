7e3dcf80b720e5e64e7fb54f0cbf389817b3841c
Munnaza <lukesh@daikimedia.com>
2026-06-02 12:35:45 +0500

    nexus.config.js updated

--- nexus.config.js
+++ nexus.config.js
@@ -2,17 +2,18 @@ module.exports = {
   apps: [
     {
       name: "nexus-clinic-malaysia",
-
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
 
-      instances: "max",
+      instances: 2,
       exec_mode: "cluster",
 
       autorestart: true,
       watch: false,
 
-      max_memory_restart: "500M",
+      max_memory_restart: "1G",
+
+      node_args: "--max-old-space-size=900",
 
       env: {
         NODE_ENV: "production"
@@ -20,5 +21,4 @@ module.exports = {
     }
   ]
 };
-
 
