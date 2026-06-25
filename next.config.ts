8eb03740572f54089e083878c4d18a4aae389374
Munnaza <lukesh@daikimedia.com>
2026-06-25 18:40:43 +0500

    nextconfig updated _next/statoc

--- next.config.ts
+++ next.config.ts
@@ -108,6 +108,15 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      {
+        source: "/_next/static/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
     ];
   },
 
