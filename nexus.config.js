5b5de069a2eeb601fae703d89870d368d0f878c6
Munnaza <lukesh@daikimedia.com>
2026-06-26 16:36:10 +0500

    nexus.config.js is updated

--- nexus.config.js
+++ nexus.config.js
@@ -4,21 +4,15 @@ module.exports = {
       name: "nexus-clinic-malaysia",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
-
-      instances: 2,
+      instances: "max",          //from instances: 2,
       exec_mode: "cluster",
-
       autorestart: true,
       watch: false,
-
-      max_memory_restart: "1G",
-
-      node_args: "--max-old-space-size=900",
-
+      max_memory_restart: "2G",  // from max_memory_restart: "1G",
+      node_args: "--max-old-space-size=2048", // from node_args: "--max-old-space-size=900",
       env: {
         NODE_ENV: "production"
       }
     }
   ]
 };
-
